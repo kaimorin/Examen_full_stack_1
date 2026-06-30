373be165f48b7e24a32b311476e5bd07ea1e034b
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 10:37:14 -0400

    repository creado

---
+++ reseñas/src/main/java/cl/duoc/resenas/repository/ReseñaRepository.java
@@ -0,0 +1,9 @@
+package cl.duoc.resenas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.duoc.resenas.model.Reseña;
+
+public interface ReseñaRepository extends JpaRepository<Reseña, Long>{
+
+}
